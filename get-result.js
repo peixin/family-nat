db968f0793957514f2ccde94cf341c3c6aef73a7
liupeixin <fengyi.mail@gmail.com>
2022-01-19 00:01:47 +0800

    fix: update get naat result logic

--- get-result.js
+++ get-result.js
@@ -1,7 +1,16 @@
-import { usersInfo, familyInfo } from "./js/data.js";
+import dotenv from "dotenv";
 import axios from "axios";
-import { fileURLToPath } from 'url';
+import crypto from "crypto";
+import { familyInfo } from "./js/data.js";
+import { fileURLToPath } from "url";
 
+dotenv.config();
+
+// https://yqpt.xa.gov.cn/nrt/js/request-sign.min.js?y=202201170330
+// official why hardcode uuid? expose the private key on the client?
+// I had to hide it
+const UUID = process.env.UUID;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const parseResult = ({ data }) => {
   if (!data || !data.length) {
@@ -17,19 +26,53 @@ const parseResult = ({ data }) => {
   }
 };
 
+const sign = (text) => {
+  const md5 = crypto.createHash("md5").update(text).digest("hex").substring(3, 28);
+  const signatureStr = crypto.createHash("sha1").update(md5).digest("hex");
+
+  // https://yqpt.xa.gov.cn/nrt/js/request-sign.min.js?y=202201170330
+  // official use https://github.com/brix/crypto-js
+  // const md5 = crypto.MD5(text).toString().substring(3, 28);
+  // const signatureStr = crypto.SHA1(md5).toString();
+
+  return signatureStr;
+};
+
 const getNATResult = async (userName, userId) => {
+  const timestamp = Math.floor(Date.now() / 1000);
+  const signData = {
+    cardNum: userId,
+    driverName: "web",
+    name: userName,
+    privateKey: PRIVATE_KEY,
+    timestamp: timestamp,
+    uuid: UUID,
+  };
+
+  const signature = sign(
+    Object.keys(signData)
+      .filter((key) => key[0] !== "_")
+      .sort()
+      .map((key) => `${key}=${signData[key]}`)
+      .join("")
+  );
+
   const options = {
     method: "POST",
     url: "https://yqpt.xa.gov.cn/prod-api/naat/open/api/getResultByCardNumAndName",
     headers: {
       "Content-Type": "application/json; charset=UTF-8",
+      Host: "yqpt.xa.gov.cn",
+      driverName: "web",
+      timestamp,
+      uuid: UUID,
+      signature,
     },
-    data: JSON.stringify({ name: userName, cardNum: userId }),
+    params: { name: userName, cardNum: userId },
   };
 
   try {
     const { data } = await axios(options);
-    // console.log(data);
 
     // const item = {
     //   name: userName,
@@ -45,12 +88,12 @@ const getNATResult = async (userName, userId) => {
 };
 
 const main = async () => {
-  for (let i = 0; i < familyInfo.members.length; i += 1) {
-    const user = usersInfo[familyInfo.members[i]];
+  for (let user of Object.values(familyInfo)) {
     await getNATResult(user.name, user.id);
   }
 };
 
+// Official entry https://yqpt.xa.gov.cn/nrt/inquire.html
 if (fileURLToPath(import.meta.url) === process.argv[1]) {
   main();
 }
